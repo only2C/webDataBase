c1aad5f8f43459b728c57e2a6e226e459e6d6e6f
only <776268047@qq.com>
2017-10-29 23:28:14 +0800

    webdatabase

---
+++ src/routes/apiRoute.js
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports = function (server) {
+    // read - If you want to divert index or home page call to swagger documentation
+    // server.route({
+    //     method: 'get',
+    //     path: '/',
+    //     handler: function (request, reply) {
+    //         return reply.redirect('/documentation');
+    //     }
+
+    // });
+
+}
